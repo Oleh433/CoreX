4464baf588c9bc833fa74dad792853af7759449a
Oleh433 <120088754+Oleh433@users.noreply.github.com>
2026-02-09 18:00:19 +0200

    COR-12/Create BookingRepository (#10)
    Co-authored-by: Oleh Pobyvanets <pobyvanets.oleh1122@vu.cdu.edu.ua>

---
+++ CoreX.Domain/RepositoryInterfaces/IBookingRepository.cs
@@ -0,0 +1,23 @@
+﻿using CoreX.Domain.Entities;
+
+namespace CoreX.Domain.RepositoryInterfaces
+{
+    public interface IBookingRepository
+    {
+        Task<Booking?> GetByIdAsync(Guid id);
+
+        Task<List<Booking>> GetAllAsync();
+
+        Task<List<Booking>> GetByUserIdAsync(Guid userId);
+
+        Task<List<Booking>> GetByClubIdAsync(Guid clubId);
+
+        Task<List<Booking>> GetActiveBookingsAsync();
+
+        Task AddAsync(Booking booking);
+
+        void Update(Booking booking);
+
+        void Delete(Booking booking);
+    }
+}
---
+++ CoreX.Infrastructure/Repositories/BookingRepository.cs
@@ -0,0 +1,90 @@
+﻿using CoreX.Domain.Entities;
+using CoreX.Domain.RepositoryInterfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreX.Infrastructure.Repositories
+{
+    public class BookingRepository : IBookingRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BookingRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Booking?> GetByIdAsync(Guid id)
+        {
+            return await _context.Bookings
+                .Include(x => x.Club)
+                .Include(x => x.Subscription)
+                .Include(x => x.Discount)
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<Booking>> GetAllAsync()
+        {
+            return await _context.Bookings
+                .Include(x => x.Club)
+                .Include(x => x.Subscription)
+                .Include(x => x.Discount)
+                .AsNoTracking()
+                .OrderByDescending(x => x.CreatedAt)
+                .ToListAsync();
+        }
+
+        public async Task<List<Booking>> GetByUserIdAsync(Guid userId)
+        {
+            return await _context.Bookings
+                .Where(x => x.UserId == userId)
+                .Include(x => x.Club)
+                .Include(x => x.Subscription)
+                .Include(x => x.Discount)
+                .AsNoTracking()
+                .OrderByDescending(x => x.CreatedAt)
+                .ToListAsync();
+        }
+
+        public async Task<List<Booking>> GetByClubIdAsync(Guid clubId)
+        {
+            return await _context.Bookings
+                .Where(x => x.ClubId == clubId)
+                .Include(x => x.Subscription)
+                .Include(x => x.Discount)
+                .AsNoTracking()
+                .OrderByDescending(x => x.CreatedAt)
+                .ToListAsync();
+        }
+
+        public async Task<List<Booking>> GetActiveBookingsAsync()
+        {
+            return await _context.Bookings
+                .Where(x =>
+                    x.Status == BookingStatus.New ||
+                    x.Status == BookingStatus.Confirmed)
+                .Include(x => x.Club)
+                .Include(x => x.Subscription)
+                .AsNoTracking()
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(Booking booking)
+        {
+            await _context.Bookings.AddAsync(booking);
+        }
+
+        public void Update(Booking booking)
+        {
+            _context.Bookings.Update(booking);
+        }
+
+        public void Delete(Booking booking)
+        {
+            _context.Bookings.Remove(booking);
+        }
+    }
