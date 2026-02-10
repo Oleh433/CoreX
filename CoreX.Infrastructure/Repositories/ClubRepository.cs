cffdb17393ff8f43fe36efb21337dc77097d468a
Oleh433 <120088754+Oleh433@users.noreply.github.com>
2026-02-10 18:07:31 +0200

    COR-18/Create Club repository (#15)
    Co-authored-by: Oleh Pobyvanets <pobyvanets.oleh1122@vu.cdu.edu.ua>

---
+++ CoreX.Infrastructure/Repositories/ClubRepository.cs
@@ -0,0 +1,58 @@
+﻿using CoreX.Domain.Entities;
+using CoreX.Domain.RepositoryInterfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreX.Infrastructure.Repositories
+{
+    public class ClubRepository : IClubRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClubRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<Club?> GetByIdAsync(Guid id)
+        {
+            return await _context.Clubs
+                .Include(x => x.Trainers)
+                .Include(x => x.Subscriptions)
+                .Include(x => x.Vacancies)
+                .Include(x => x.Memberships)
+                .FirstOrDefaultAsync(x => x.Id == id);
+        }
+
+        public async Task<List<Club>> GetAllAsync()
+        {
+            return await _context.Clubs
+                .AsNoTracking()
+                .OrderBy(x => x.Name)
+                .ToListAsync();
+        }
+
+        public async Task<List<Club>> GetByCityAsync(string city)
+        {
+            return await _context.Clubs
+                .Where(x => x.City.ToLower() == city.ToLower())
+                .AsNoTracking()
+                .OrderBy(x => x.Name)
+                .ToListAsync();
+        }
+
+        public async Task AddAsync(Club club)
+        {
+            await _context.Clubs.AddAsync(club);
+        }
+
+        public void Update(Club club)
+        {
+            _context.Clubs.Update(club);
+        }
+
+        public void Delete(Club club)
+        {
+            _context.Clubs.Remove(club);
+        }
+    }
+}
