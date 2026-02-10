79e6fb7f09591891632cd1a8d52a4af9b25fd48c
Oleh Pobyvanets <pobyvanets.oleh1122@vu.cdu.edu.ua>
2026-02-10 17:51:45 +0200

    COR-16/Create VacancyApplication repository

---
+++ CoreX.Domain/RepositoryInterfaces/IVacancyApplicationRepository.cs
@@ -0,0 +1,26 @@
+﻿using CoreX.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreX.Domain.RepositoryInterfaces
+{
+    public interface IVacancyApplicationRepository
+    {
+        Task<VacancyApplication?> GetByIdAsync(Guid id);
+
+        Task<List<VacancyApplication>> GetAllAsync();
+
+        Task<List<VacancyApplication>> GetByVacancyIdAsync(Guid vacancyId);
+
+        Task<List<VacancyApplication>> GetByApplicantIdAsync(Guid applicantId);
+
+        Task AddAsync(VacancyApplication application);
+
+        void Update(VacancyApplication application);
+
+        void Delete(VacancyApplication application);
+    }
+}
