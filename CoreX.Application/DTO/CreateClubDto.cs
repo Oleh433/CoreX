afc18860877e43ab83af511c0f1de0f88ebf03fc
Oleh Pobyvanets <pobyvanets.oleh1122@vu.cdu.edu.ua>
2026-02-10 18:25:42 +0200

    COR-19/Create CLub service

---
+++ CoreX.Application/DTO/CreateClubDto.cs
@@ -0,0 +1,21 @@
+﻿namespace CoreX.Application.DTO
+{
+    public class CreateClubDto
+    {
+        public string Name { get; set; } = default!;
+
+        public string City { get; set; } = default!;
+
+        public string Address { get; set; } = default!;
+
+        public double? Latitude { get; set; }
+
+        public double? Longitude { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? Phone { get; set; }
+
+        public string? Email { get; set; }
+    }
+}
