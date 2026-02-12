a9b36b094db5fe5cedeb8929146e64178e8d6c55
Max Ivashchenko <ivashchenko.maksym1122@vu.cdu.edu.ua>
2026-02-12 18:59:51 +0200

    COR-25/Add Vacancy service

---
+++ CoreX.Application/DTO/VacancyResponseDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreX.Application.DTO
+{
+    public class VacancyResponseDto
+    {
+        public Guid Id { get; set; }
+
+        public Guid ClubId { get; set; }
+
+        public string? ClubName { get; set; }
+
+        public string Title { get; set; } = default!;
+
+        public string Description { get; set; } = default!;
+
+        public string Requirements { get; set; } = default!;
+
+        public decimal? Salary { get; set; }
+
+        public bool IsActive { get; set; }
+
+        public int ApplicationsCount { get; set; }
+    }
+}
