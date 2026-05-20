36d03056a01487f931890081b8fcd899afcc655e
Max Ivashchenko <maximivashchenko05@gmail.com>
2026-05-20 16:21:05 +0300

    Scaffold Razor Pages view imports and SharedResource

---
+++ CoreX/Resources/SharedResource.cs
@@ -0,0 +1,7 @@
+namespace CoreX.Resources;
+
+// Marker class for IStringLocalizer<SharedResource>.
+// Strings are stored in Resources/SharedResource.{uk,en}.resx.
+public class SharedResource
+{
+}
